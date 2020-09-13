88d66d1e41a7a634720ba157e95404865f099417
ValdikSS <iam@valdikss.org.ru>
2020-09-13 22:11:51 +0300

    Fix typo

--- config/config.sh
+++ config/config.sh
@@ -3,7 +3,7 @@
 # HTTPS (TLS) proxy address
 PACHTTPSHOST='proxy-ssl.antizapret.prostovpn.org:3143'
 
-# Usual proxy address
+# Regular proxy address
 PACPROXYHOST='proxy-nossl.antizapret.prostovpn.org:29976' 
 
 PACFILE="result/proxy-host-ssl.pac"
